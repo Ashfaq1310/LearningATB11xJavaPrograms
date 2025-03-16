51bca87e1f89a42354c92a6dd34c49f8c025df4c
Ashfaq hussain <ashfaq.mah1310@gmail.com>
2025-03-16 14:49:45 +0530

    10th March class  Multi_methods.java

---
+++ src/ex_01_JavaBasic/Multi_methods.java
@@ -0,0 +1,14 @@
+package ex_01_JavaBasic;
+
+public class Multi_methods {
+    // this is final code that i have written
+    public static void main(String[] args) {
+        System.out.println("Final");
+    }
+
+    public static void main(String args) {
+    }
+
+    public static void main(int args) {
+    }
+}
