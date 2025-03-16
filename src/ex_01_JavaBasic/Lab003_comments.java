83f607c2ec3377ca71927e5e55bb848363110e9c
Ashfaq hussain <ashfaq.mah1310@gmail.com>
2025-03-16 13:29:12 +0530

    10th March class

---
+++ src/ex_01_JavaBasic/Lab003_comments.java
@@ -0,0 +1,22 @@
+package ex_01_JavaBasic;
+// Hi World
+public class Lab003_comments {
+    public static void main(String[] args) {
+        // Attaining the Class
+        // MacBook Air
+        System.out.println("Comment");
+        // Trying comment
+        // Trying comment
+
+        System.out.println("Comment1");
+        System.out.println("Comment2");
+        System.out.println("Comment3");
+        System.out.println("Comment4");
+// Trying comment
+        // Trying comment
+    }
+
+
+}
+
+
---
+++ src/ex_01_JavaBasic/Lab004_Comment_multi.java
@@ -0,0 +1,14 @@
+package ex_01_JavaBasic;
+
+public class Lab004_Comment_multi {
+    /*
+    Hello :- World
+    Owner :- Ashfaq
+    OS :- IOS
+    System :- MacBook Air
+     */
+    public static void main(String[] args) {
+        System.out.println("Multi Comments");
+    }
+    // delete
+}
