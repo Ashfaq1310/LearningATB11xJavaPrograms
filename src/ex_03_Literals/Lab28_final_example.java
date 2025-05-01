dfbec906b51bb9c0fe50e976db610f8fac4a1782
Ashfaq hussain <ashfaq.mah1310@gmail.com>
2025-05-01 23:00:25 +0530

    12th March class  Data Types

---
+++ src/ex_03_Literals/Lab28_final_example.java
@@ -0,0 +1,17 @@
+package ex_03_Literals;
+
+public class Lab28_final_example {
+    public static void main(String[] args) {
+
+        int local;
+        local=10;
+        local=20;
+        local=30;
+        local=50;
+        System.out.println(local);
+
+        final float PI=3.14f;
+
+        System.out.println(PI);
+    }
+}
